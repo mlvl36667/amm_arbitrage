f3423362d2de473c6f6f489739f9c32033065a99
Your Name <hikari.code@gmail.com>
2025-05-27 08:04:28 +0200

    adding integrator.cpp

--- integrator.cpp
+++ integrator.cpp
@@ -649,7 +649,8 @@ int main() {
                                  << "_GM" << gamma_minus_param
                                  << "_CS" << const_sigma_param
                                  << "_CM" << const_mu_param
-                                 << "_NPT" << n_points_grid; // <<< ADDED N_POINTS_GRID HERE
+                                 << "_NPT" << n_points_grid // <<< ADDED N_POINTS_GRID HERE
+                                 << "_NIT" << num_iterations; // NUM_ITERATIONS
               std::string filename_body = filename_body_sstr.str();
               std::replace(filename_body.begin(), filename_body.end(), '.', 'p'); // Replace '.' with 'p'
               std::string ratio_filename = filename_body + ".txt";
